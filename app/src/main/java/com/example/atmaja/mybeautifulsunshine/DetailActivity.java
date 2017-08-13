3dec8470b8f0979b934091dec26ebe6e87849e4c
prajogoatmaja <prajogo.atmaja@gmail.com>
2017-08-13 12:05:07 +0700

    Weather Adapter, Card View, etc.

---
+++ app/src/main/java/com/example/atmaja/mybeautifulsunshine/DetailActivity.java
@@ -0,0 +1,8 @@
+package com.example.atmaja.mybeautifulsunshine;
+
+/**
+ * Created by atmaja on 13/08/17.
+ */
+
+class DetailActivity {
+}
---
+++ app/src/main/java/com/example/atmaja/mybeautifulsunshine/WeatherAdapter.java
@@ -0,0 +1,10 @@
+package com.example.atmaja.mybeautifulsunshine;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by atmaja on 13/08/17.
+ */
+
+public class WeatherAdapter extends RecyclerView.Adapter {
+}
